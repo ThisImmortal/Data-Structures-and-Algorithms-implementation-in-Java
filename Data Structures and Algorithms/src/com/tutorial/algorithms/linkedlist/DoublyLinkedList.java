136e89bb2bcd837e54672d40a2ee0b8027b7a6c4
Zaur <zaurrhuseynzade@gmail.com>
2021-10-16 22:50:30 +0400

    Doubly LinkedList: adding implementations of insertFirst(), insertLast(), deleteFirst(), deleteLast()

--- Data Structures and Algorithms/src/com/tutorial/algorithms/linkedlist/DoublyLinkedList.java
+++ Data Structures and Algorithms/src/com/tutorial/algorithms/linkedlist/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package com.tutorial.algorithms.linkedlist;
 
+import java.util.NoSuchElementException;
+
 public class DoublyLinkedList {
 	
 	private ListNode head;
@@ -32,20 +34,7 @@ public class DoublyLinkedList {
 		return length;
 	}
 	
-	
-	public void insertLast(int value) {
-		ListNode newNode = new ListNode(value);
-		if(isEmpty()) {
-			head=newNode;
-		}
-		else {
-			tail.next = newNode;
-			
-		}
-		newNode.previous = tail;
-		tail = newNode;
-		length++;
-	}
+
 
 	//Display Doubly LinkedList in forward direction
 	public void displayForward() {
@@ -77,14 +66,97 @@ public class DoublyLinkedList {
 		System.out.println("null");
 	}
 	
+	//Insert node at the beginning of a Doubly LinkedList
+	public void insertFirst(int value) {
+		ListNode newNode = new ListNode(value);
+		
+		if(isEmpty()) {
+			tail = newNode;
+		}
+		else {
+			head.previous = newNode;
+		}
+		newNode.next = head;
+		head = newNode;
+		length++;
+	}
+	
+	//Insert node at the beginning of a Doubly LinkedList
+	public void insertLast(int value) {
+		ListNode newNode = new ListNode(value);
+		
+		if(isEmpty()) {
+			head = newNode;
+		}		
+		else {
+			tail.next = newNode;
+			newNode.previous = tail;
+		}
+		tail = newNode;
+		length++;
+	}
+	
+	//Delete first node in a Doubly LinkedList
+	public ListNode deleteFirst() {
+		if(isEmpty()) {
+			throw new NoSuchElementException("No such element!");}
+			
+			ListNode temp = head;
+			if(head == tail) {
+				tail = null;
+			}
+			else {
+				temp.next.previous = null;
+			}
+			head = head.next;
+			temp.next = null;
+			length--;
+		
+		
+		return temp;
+	}
+	
+	//Delete last node in a Doubly LinkedList
+	public ListNode deleteLast() {
+		if(isEmpty()) {
+			throw new NoSuchElementException("No such element!");
+		}
+		
+		ListNode temp = tail;
+		if(head == tail) {
+			head = null;
+		}
+		else {
+			tail.previous.next = null;
+			
+		}
+		tail = tail.previous;
+		temp.previous = null;
+		length--;
+		
+		return temp;
+	}
+	
 	public static void main(String[]args) {
 		DoublyLinkedList dll = new DoublyLinkedList();
-		System.out.println(dll.isEmpty());
-		System.out.println(dll.length);
-		dll.insertLast(2);
-		dll.insertLast(5);
-		dll.insertLast(8);
-		dll.displayForward();
-		dll.displayBackward();
+//		System.out.println(dll.isEmpty());
+//		System.out.println(dll.length);
+//		dll.insertFirst(2);
+//		dll.insertFirst(5);
+//		dll.insertFirst(8);
+//		dll.displayForward();
+//		dll.displayBackward();
+//		dll.insertFirst(3);
+//		dll.insertLast(4);
+//		dll.displayForward();
+//		dll.deleteFirst();
+//		dll.displayForward();
+//		dll.deleteLast();
+//		dll.deleteFirst();
+//		dll.deleteFirst();
+//		dll.deleteFirst();
+//		dll.deleteFirst();
+//		
+//		dll.displayForward();
 	}
 }
